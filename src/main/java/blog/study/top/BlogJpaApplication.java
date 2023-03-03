7a5bd2cc87d63030c63e49cc29adf8919027c453
inwookjung <inwook.jung@kr.doubledown.com>
2023-03-03 23:06:46 +0900

    Initial commit

---
+++ src/main/java/blog/study/top/BlogJpaApplication.java
@@ -0,0 +1,13 @@
+package blog.study.top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogJpaApplication.class, args);
+	}
+
+}
