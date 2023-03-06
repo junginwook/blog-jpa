faa61b17bc10700b9dfba42af5163df7803cbba9
inwookjung <inwook.jung@kr.doubledown.com>
2023-03-07 01:03:52 +0900

    연관관계 없이 join 하기

---
+++ src/main/java/blog/study/top/연관관계없이Join조회하기/repository/TeacherRepository230306CustomImpl.java
@@ -0,0 +1,2 @@
+package blog.study.top.연관관계없이Join조회하기.repository;public class TeacherRepository230306CustomImpl {
+}
---
+++ src/test/java/blog/study/top/연관관계없이Join조회하기/repository/TeacherRepository230306Test.java
@@ -0,0 +1,4 @@
+import static org.junit.jupiter.api.Assertions.*;
+class TeacherRepository230306Test {
+  
+}
